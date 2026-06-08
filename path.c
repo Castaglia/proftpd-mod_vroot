b9ba20f2fc7f7629a660d6c8dc3bc87ab707f299
TJ Saunders <tj@castaglia.org>
2026-06-08 12:00:44 -0700

    Issue #53: Update the license text, per FSF recommendations.

--- path.c
+++ path.c
@@ -1,6 +1,6 @@
 /*
  * ProFTPD: mod_vroot Path API
- * Copyright (c) 2016-2025 TJ Saunders
+ * Copyright (c) 2016-2026 TJ Saunders
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,8 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1335, USA.
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
  *
  * As a special exemption, the respective copyright holders give permission
  * to link this program with OpenSSL, and distribute the resulting
